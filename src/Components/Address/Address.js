f50c54478bc9901e1bdf5cf6152dfa2a3fe3bee9
feden2906 <feden2906@gmail.com>
2021-08-11 15:06:52 +0300

    add new component

---
+++ src/Components/Address/Address.js
@@ -0,0 +1,8 @@
+export const Address = ({address}) => {
+    const {street, suite, city, zipcode, geo} = address;
+    return (
+        <div>
+
+        </div>
+    );
+}
--- src/Components/Product/Character.js
+++ src/Components/Product/Character.js
@@ -1,12 +1,14 @@
+import { Address } from "../Address/Address";
+
 export default function Obj ({user}){
-    let {id, name, username} = user
+    let {id, name, username, address} = user
     return(
         <div>
             <div>
                 <h2>Id: {id}</h2>
                 <h2>name: {name}</h2>
                 <h2>Username: {username}</h2>
-                {/*<h2>Address: {address}</h2>*/}
+                <Address address={address}/>
 
                 <hr/>
             </div>
