61f25231a63138fc37ad03f3e8b1ef189eb340e3
Markus5555 <pupoks123@gmail.com>
2021-08-26 15:15:28 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    withRouter
+} from "react-router-dom";
 
 export default function App() {
     return (
-        <div>
+        <Router>
+            <div>
+                <Link to={'/users'}>Users Page</Link>
+                <Link to={'/posts'}>Posts Page</Link>
+                <Link to={'/comments'}>Comments Page</Link>
 
-        </div>
+            </div>
+        </Router>
     )
 }
